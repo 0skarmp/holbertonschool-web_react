94adbc33dd57242d365580dbef74ae9ff1103b47
OSKAR <omoralespj@gmail.com>
2023-11-26 02:43:49 -0500

    task3

--- react_intro/task_3/dashboard/src/App.js
+++ react_intro/task_3/dashboard/src/App.js
@@ -1,27 +1,29 @@
-import holberton_logo from './holberton_logo.jpg';
-import { getFullYear, getFooterCopy } from './utils';
+import HolbertonLogo from './holberton_logo.jpg';
 import './App.css';
-
+import { getFullYear, getFooterCopy } from './utils';
 
 function App() {
   return (
     <>
       <header className="App-header">
-        <img src={holberton_logo} alt="logo" />
+        <img src={HolbertonLogo} alt="logo-holberton" />
         <h1>School dashboard</h1>
       </header>
-
-      <div className="App-body">
+      <main className="App-body">
         <p>Login to access the full dashboard</p>
-        <label htmlFor='email'>email: </label>
-        <input type='email'></input>
-        <label htmlFor='password'>password: </label>
-        <input type='password'></input>
-        <button>ok</button>
-
-      </div>
-      <footer className="App-footer"><p>Copyright {getFullYear()} - {getFooterCopy("isIndex")}</p></footer>
-    </>);
+        <form>
+          <label htmlFor="email">email:</label>
+          <input type='email' id='email' autoComplete='on'/>
+          <label htmlFor="password">password:</label>
+          <input type='password' id='password' autoComplete='on'/>
+          <button>OK</button>
+        </form>
+      </main>
+      <footer className="App-footer">
+      <p>Copyright { getFullYear()} - {getFooterCopy('isIndex')}</p>
+      </footer>
+    </>
+  );
 }
 
 export default App;
--- react_intro/task_3/dashboard/src/Notifications.js
+++ react_intro/task_3/dashboard/src/Notifications.js
@@ -1,6 +1,50 @@
-import './Notifications.css'
 import React from 'react';
+import './Notifications.css';
+import CloseIcon from './close-icon.png';
+import { getLatestNotification } from './utils';
 
-export const X =  () => {
-    return <div className='Notifications'><p>Here is the list of notifications</p></div>
-}
+export const X = () => {
+	return (
+		<div className="Notifications">
+			<button
+				style={{
+					color: '#3a3a3a',
+					fontWeight: 'bold',
+					background: 'none',
+					border: 'none',
+					fontSize: '15px',
+					position: 'absolute',
+					right: '3px',
+					top: '3px',
+					cursor: 'pointer',
+					outline: 'none',
+				}}
+				aria-label="Close"
+				onClick={(e) => {
+					console.log('Close button has been clicked');
+				}}
+			>
+				<img src={CloseIcon} alt="close icon" />
+			</button>
+			<p>
+				Here is the list of notifications
+			</p>
+			<ul>
+				<li data-priority="default">
+					New course available
+				</li>
+				<li data-priority="urgent">
+					New resume available
+				</li>
+				<li
+					data-priority="urgent"
+					dangerouslySetInnerHTML={{__html: getLatestNotification()}}
+				>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default X;
+
