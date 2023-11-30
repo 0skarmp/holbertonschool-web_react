7aabf0a1f9c25a73913e52db9ad6de217ca792ee
oscar morales <131509024+0skarmp@users.noreply.github.com>
2023-11-29 19:07:33 -0500

    Delete react_props/task_0/dashboard directory

--- react_props/task_0/dashboard/src/Notifications.js
+++
@@ -1,8 +0,0 @@
-import './Notifications.css'
-import React from 'react';
-
-const X =  () => {
-    return <div className='Notifications'><p>Here is the list of notifications</p></div>
-}
-
-export default X; 
